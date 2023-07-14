611e8a089a0ca73d919ea5657e1cace6c38ff388
abdurahmanasgf <abdurrahmanasgf@gmail.com>
2023-07-14 10:47:43 +0200

    1st commit run code

--- run_code.py
+++ run_code.py
@@ -2,5 +2,5 @@ import web_scrap_glassdoor as gs
 import pandas as pd
 
 path = "C:/Users/user/Desktop/AmanPythonLearning/Data Analysis/web_scrapping"
-df = gs.get_jobs('data scientist', 15, False, path, 15)
-df.to_excel('./papa.xlsx')
+df = gs.get_jobs(1000, False, path, 15)
+df.to_csv('data_analyst_jobs.csv', index= False)
